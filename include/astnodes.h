cd0163726fa0e772273713354c77e68fa829f9c5
Ali Hamza <ali.mhamza06@gmail.com>
2025-12-17 23:54:22 +0400

    Make AST node classes for new compiler.
    The compiler will receive an AST rather than a stream of tokens, but will compile to nearly identical bytecode (not accounting for optimizations).

---
+++ include/astnodes.h
@@ -0,0 +1,214 @@
+#pragma once
+#include "common.h"
+#include "token.h"
+#include <memory>
+#include <vector>
+
+namespace AST
+{
+    namespace Statement { struct Stmt; }
+    namespace Expression { struct Expr; }
+}
+
+using StmtUP        = std::unique_ptr<AST::Statement::Stmt>;
+using ExprUP        = std::unique_ptr<AST::Expression::Expr>;
+using StmtVec       = std::vector<StmtUP>;
+using ExprVec       = std::vector<ExprUP>;
+
+#define StmtUP(ptr) static_cast<StmtUP>(ptr)
+#define ExprUP(ptr) static_cast<ExprUP>(ptr)
+#define UP(type)    std::unique_ptr<type>
+
+namespace AST
+{   
+    namespace Statement
+    {
+        enum StmtType : ui8
+        {
+            S_VAR_DECL,
+            S_FUN_DECL,
+            S_CLASS_DECL,
+            S_RETURN_STMT,
+            S_LOOP_STMT,
+            S_EXPR_STMT,
+            S_BLOCK_STMT
+        };
+
+        struct Stmt
+        {
+            StmtType type;
+
+            Stmt(StmtType type);
+            virtual ~Stmt() = default;
+        };
+
+        struct VarDecl : public Stmt
+        {
+            TokenType declType;
+            Token name;
+            ExprUP init;
+
+            VarDecl(TokenType declType, Token& name, ExprUP init);
+        };
+
+        struct FunDecl : public Stmt
+        {
+            Token name;
+            vT params;
+            StmtVec body;
+
+            FunDecl(Token name, vT& params, StmtVec& body);
+        };
+
+        struct ClassDecl : public Stmt
+        {
+            Token name;
+            vT fields;
+            StmtVec methods;
+
+            ClassDecl(Token& name, vT& fields, StmtVec& methods);
+        };
+
+        struct ReturnStmt : public Stmt
+        {
+            Token keyword;
+            ExprUP expr;
+
+            ReturnStmt(Token& keyword, ExprUP expr);
+        };
+
+        struct LoopStmt : public Stmt
+        {
+            ExprUP condition;
+            StmtVec body;
+
+            LoopStmt(ExprUP condition, StmtVec& body);
+        };
+
+        struct ExprStmt : public Stmt
+        {
+            ExprUP expr;
+
+            ExprStmt(ExprUP expr);
+        };
+
+        struct BlockStmt : public Stmt
+        {
+            StmtVec block;
+
+            BlockStmt(StmtVec& block);
+        };
+    };
+
+    namespace Expression
+    {
+        enum ExprType : ui8
+        {
+            E_ASSIGN_EXPR,
+            E_LOGIC_EXPR,
+            E_COMPARE_EXPR,
+            E_BIT_EXPR,
+            E_SHIFT_EXPR,
+            E_BINARY_EXPR,
+            E_UNARY_EXPR,
+            E_CALL_EXPR,
+            E_VAR_EXPR,
+            E_LITERAL_EXPR
+        };
+        
+        struct Expr
+        {
+            ExprType type;
+
+            Expr(ExprType type);
+            virtual ~Expr() = default;
+        };
+
+        struct AssignExpr : public Expr
+        {
+            ExprUP target;
+            ExprUP value;
+
+            AssignExpr(ExprUP target, ExprUP value);
+        };
+
+        // All five are effectively the same,
+        // but it would simplify things to keep them
+        // as separate node types.
+
+        struct LogicExpr : public Expr
+        {
+            ExprUP left;
+            TokenType oper;
+            ExprUP right;
+
+            LogicExpr(ExprUP left, TokenType oper, ExprUP right);
+        };
+
+        struct CompareExpr : public Expr
+        {
+            ExprUP left;
+            TokenType oper;
+            ExprUP right;
+
+            CompareExpr(ExprUP left, TokenType oper, ExprUP right);
+        };
+
+        struct BitExpr : public Expr
+        {
+            ExprUP left;
+            TokenType oper;
+            ExprUP right;
+
+            BitExpr(ExprUP left, TokenType oper, ExprUP right);
+        };
+
+        struct ShiftExpr : public Expr
+        {
+            ExprUP left;
+            TokenType oper;
+            ExprUP right;
+
+            ShiftExpr(ExprUP left, TokenType oper, ExprUP right);
+        };
+
+        struct BinaryExpr : public Expr
+        {
+            ExprUP left;
+            TokenType oper;
+            ExprUP right;
+
+            BinaryExpr(ExprUP left, TokenType oper, ExprUP right);
+        };
+
+        struct UnaryExpr : public Expr
+        {
+            TokenType oper;
+            ExprUP expr;
+
+            UnaryExpr(TokenType oper, ExprUP expr);
+        };
+
+        struct CallExpr : public Expr
+        {
+            ExprUP callee;
+            ExprVec args;
+
+            CallExpr(ExprUP callee, ExprVec& args);
+        };
+
+        struct VarExpr : public Expr
+        {
+            Token name;
+
+            VarExpr(Token& name);
+        };
+
+        struct LiteralExpr : public Expr
+        {
+            Token value;
+
+            LiteralExpr(Token& value);
+        };
+    };
+}
---
+++ src/astnodes.cpp
@@ -0,0 +1,81 @@
+#include "../include/astnodes.h"
+using namespace AST::Statement;
+using namespace AST::Expression;
+
+// Statement constructors.
+
+Stmt::Stmt(StmtType type) :
+    type(type) {}
+
+VarDecl::VarDecl(TokenType declType, Token& name, ExprUP init) :
+    Stmt(S_VAR_DECL),
+    declType(declType), name(name), init(std::move(init)) {}
+
+FunDecl::FunDecl(Token name, vT& params, StmtVec& body) :
+    Stmt(S_FUN_DECL),
+    name(name), params(params), body(std::move(body)) {}
+
+ClassDecl::ClassDecl(Token& name, vT& fields, StmtVec& methods) :
+    Stmt(S_CLASS_DECL),
+    name(name), fields(fields), methods(std::move(methods)) {}
+
+ReturnStmt::ReturnStmt(Token& keyword, ExprUP expr) :
+    Stmt(S_RETURN_STMT),
+    keyword(keyword), expr(std::move(expr)) {}
+
+LoopStmt::LoopStmt(ExprUP condition, StmtVec& body) :
+    Stmt(S_LOOP_STMT),
+    condition(std::move(condition)), body(std::move(body)) {}
+
+ExprStmt::ExprStmt(ExprUP expr) :
+    Stmt(S_EXPR_STMT),
+    expr(std::move(expr)) {}
+
+BlockStmt::BlockStmt(StmtVec& block) :
+    Stmt(S_BLOCK_STMT),
+    block(std::move(block)) {}
+
+// Expression constructors.
+
+Expr::Expr(ExprType type) :
+    type(type) {}
+
+AssignExpr::AssignExpr(ExprUP target, ExprUP value) :
+    Expr(E_ASSIGN_EXPR),
+    target(std::move(target)), value(std::move(value)) {}
+
+LogicExpr::LogicExpr(ExprUP left, TokenType oper, ExprUP right) :
+    Expr(E_LOGIC_EXPR),
+    left(std::move(left)), oper(oper), right(std::move(right)) {}
+
+CompareExpr::CompareExpr(ExprUP left, TokenType oper, ExprUP right) :
+    Expr(E_COMPARE_EXPR),
+    left(std::move(left)), oper(oper), right(std::move(right)) {}
+
+BitExpr::BitExpr(ExprUP left, TokenType oper, ExprUP right) :
+    Expr(E_BIT_EXPR),
+    left(std::move(left)), oper(oper), right(std::move(right)) {}
+
+ShiftExpr::ShiftExpr(ExprUP left, TokenType oper, ExprUP right) :
+    Expr(E_SHIFT_EXPR),
+    left(std::move(left)), oper(oper), right(std::move(right)) {}
+
+BinaryExpr::BinaryExpr(ExprUP left, TokenType oper, ExprUP right) :
+    Expr(E_BINARY_EXPR),
+    left(std::move(left)), oper(oper), right(std::move(right)) {}
+
+UnaryExpr::UnaryExpr(TokenType oper, ExprUP expr) :
+    Expr(E_UNARY_EXPR),
+    oper(oper), expr(std::move(expr)) {}
+
+CallExpr::CallExpr(ExprUP callee, ExprVec& args) :
+    Expr(E_CALL_EXPR),
+    callee(std::move(callee)), args(std::move(args)) {}
+
+VarExpr::VarExpr(Token& name) :
+    Expr(E_VAR_EXPR),
+    name(name) {}
+
+LiteralExpr::LiteralExpr(Token& value) :
+    Expr(E_LITERAL_EXPR),
+    value(value) {}
