6976195fc4f80d81780a05f429359a7c019a81cb
Ali Hamza <ali.mhamza06@gmail.com>
2026-01-01 22:30:49 +0400

    Update token compiler to match features in AST compiler.
    Only need to diagnose and fix minor bugs in this (or both) compiler(s) before moving forward to next steps.

--- include/compiler.h
+++ include/compiler.h
@@ -4,6 +4,9 @@
 #include "token.h"
 #include <string_view>
 #include <string>
+#include <vector>
+
+class TokCompVarsWrapper;
 
 class Compiler
 {
@@ -16,13 +19,24 @@ class Compiler
         Token previousTok;
         Token currentTok;
         vT::const_iterator it;
-        static constexpr int regSize = 256;
         ui8 previousReg;
         ui8 currentReg;
+        // The last register that contains a variable (initially 0).
+        ui8 lastVarReg;
+        ui8 scope; // Our current lexical scope depth.
+        std::vector<std::vector<std::string>> varScopes;
+        TokCompVarsWrapper* varsWrapper;
 
         // For registers.
+
         void freeReg();
         void reserveReg();
+
+        // For variables.
+
+        void defVar(std::string name, ui8 reg);
+        ui8* getVarSlot(const Token& token);
+        void popScope();
 
         // Utilities.
 
@@ -37,23 +51,48 @@ class Compiler
         void reset();
         void matchType(std::string_view message = "");
 
-        // Recursive descent parsing functions.
+        // Condensed compiling function.
+        void compileDescent(void (Compiler::*func)(), TokenType tok, Opcode op);
 
-        void declaration();
+        // Recursive descent parsing functions.
 
         // Declarations.
 
+        void declaration();
         void varDecl();
+        void funDecl();
+        void classDecl();
+
+        // Statements.
 
         void statement();
+        void returnStmt();
+        void loopStmt();
+        void blockStmt();
         void exprStmt();
 
+        // Expressions.
+
         void expression();
+        void assignment();
+        void logicOr();
+        void logicAnd();
+        void equality();
+        void comparison();
+        void bitOr();
+        void bitXor();
+        void bitAnd();
+        void shift();
         void sum();
         void product();
+        void unary();
+        void exponent();
+        void call();
         void primary();
     
     public:
         Compiler();
+        ~Compiler();
+
         ByteCode& compile(const vT& tokens);
 };
--- src/compiler.cpp
+++ src/compiler.cpp
@@ -1,16 +1,35 @@
+#ifndef COMP_AST
+
+#include "chainTable.h"
 #include "../include/compiler.h"
 #include "../include/common.h"
 #include "../include/error.h"
 #include "../include/object.h"
 #include "../include/opcodes.h"
 #include "../include/token.h"
+#include "../include/vartable.h"
 #include <cstring>
 #include <iostream>
 #include <memory>
 #include <variant>
 
+class TokCompVarsWrapper
+{
+    public:
+        chainTable<VarEntry, ui8, VarHasher> vars;
+
+        TokCompVarsWrapper() = default;
+};
+
 Compiler::Compiler() :
-    previousReg(0), currentReg(1) {}
+    previousReg(0), currentReg(1), lastVarReg(0),
+    scope(0), varScopes(1),
+    varsWrapper(new TokCompVarsWrapper) {}
+
+Compiler::~Compiler()
+{
+    delete varsWrapper;
+}
 
 void Compiler::freeReg()
 {
@@ -24,9 +43,27 @@ void Compiler::reserveReg()
     currentReg++;
 }
 
+void Compiler::defVar(std::string name, ui8 reg)
+{
+    varsWrapper->vars[{name, scope}] = reg;
+    varScopes.back().push_back(name);
+}
+
+ui8* Compiler::getVarSlot(const Token& token)
+{
+    VarEntry entry(token.text, scope);
+    return varsWrapper->vars.get(entry);
+}
+
+void Compiler::popScope()
+{
+    for (std::string& var : varScopes.back())
+        varsWrapper->vars.remove({var, scope});
+}
+
 void Compiler::nextTok()
 {
-    if (currentTok.type != TOKEN_EOF)
+    if (currentTok.type != TOK_EOF)
     {
         previousTok = currentTok;
         currentTok = *(++it);
@@ -86,6 +123,21 @@ void Compiler::matchType(std::string_view message /* = "" */)
         throw CompileError(currentTok, std::string(message));
 }
 
+void Compiler::compileDescent(void (Compiler::*func)(),
+    TokenType tok, Opcode op)
+{
+    ui8 firstOper = previousReg;
+    (this->*func)();
+
+    while (consumeTok(tok))
+    {
+        ui8 secondOper = previousReg;
+        (this->*func)();
+        code.addOp(op, firstOper, firstOper, secondOper);
+        freeReg();
+    }
+}
+
 void Compiler::declaration()
 {
     if (consumeToks(TOK_MAKE, TOK_FIX))
@@ -100,35 +152,169 @@ void Compiler::varDecl()
     consumeTok(TOK_DEF); // In case it's there.
 
     matchError(TOK_IDENTIFIER, "Expect variable name.");
-    // Token name = previousTok;
+    Token name = previousTok;
 
     if (consumeTok(TOK_COLON))
         matchType("Expect variable type.");
 
     if (consumeTok(TOK_EQUAL))
-    {
-        // ...
-    }
+        expression();
     else if (declType == TOK_FIX)
         throw CompileError(currentTok,
             "Initializer required for fixed-value variable.");
+    else
+    {
+        code.loadReg(lastVarReg, OP_NULL);
+        reserveReg();
+    }
     matchError(TOK_SEMICOLON, "Expect ';' after variable declaration.");
+
+    defVar(
+        std::string(name.text),
+        lastVarReg
+    );
+    lastVarReg++;
 }
 
 void Compiler::statement()
 {
-    exprStmt();
+    if (consumeTok(TOK_LEFT_BRACE))
+        blockStmt();
+    else
+        exprStmt();
+}
+
+void Compiler::blockStmt()
+{
+    scope++;
+    ui8 origVarReg = lastVarReg;
+    while (!checkTok(TOK_RIGHT_BRACE) && !checkTok(TOK_EOF))
+        declaration();
+    matchError(TOK_RIGHT_BRACE, "Expect '}' after block.");
+    popScope();
+    scope--;
+    previousReg -= (lastVarReg - origVarReg);
+    currentReg -= (lastVarReg - origVarReg);
+    lastVarReg = origVarReg;
 }
 
 void Compiler::exprStmt()
 {
     expression();
     matchError(TOK_SEMICOLON, "Expect ';' after expression.");
+    freeReg();
 }
 
 void Compiler::expression()
 {
+    assignment();
+}
+
+void Compiler::assignment()
+{
+    // For the time being, we just look for direct
+    // identifier names.
+    // This would need to be modified when other types
+    // of "variables" are introduced, like class fields.
+
+    // Bit of a hack.
+    const Token& firstTok = currentTok;
+    const Token& secondTok = *(it + 1);
+
+    if ((firstTok.type == TOK_IDENTIFIER)
+        && (secondTok.type == TOK_EQUAL))
+    {
+        nextTok();
+        ui8* slot = getVarSlot(previousTok);
+        nextTok();
+        if (slot != nullptr)
+        {
+            ui8 value = previousReg;
+            expression(); // Does not consume the ';'.
+            code.addOp(OP_SET_VAR, *slot, value);
+        }
+        freeReg();
+    }
+    else
+        logicOr();
+}
+
+void Compiler::logicOr()
+{
+    compileDescent(&Compiler::logicAnd, TOK_BAR_BAR, OP_OR);
+}
+
+void Compiler::logicAnd()
+{
+    compileDescent(&Compiler::equality, TOK_AMP_AMP, OP_AND);
+}
+
+void Compiler::equality()
+{
+    compileDescent(&Compiler::comparison, TOK_EQ_EQ, OP_EQUAL);
+}
+
+void Compiler::comparison()
+{
+    ui8 firstOper = previousReg;
+    bitOr();
+
+    while (consumeToks(TOK_GT, TOK_GT_EQ, TOK_LT, TOK_LT_EQ))
+    {
+        TokenType oper = previousTok.type;
+        ui8 secondOper = previousReg;
+        bitOr();
+
+        switch (oper)
+        {
+            case TOK_GT:
+            case TOK_LT_EQ:
+                code.addOp(OP_GT, firstOper, firstOper, secondOper);
+                break;
+            case TOK_LT:
+            case TOK_GT_EQ:
+                code.addOp(OP_LT, firstOper, firstOper, secondOper);
+                break;
+            default: UNREACHABLE();
+        }
+
+        if ((oper == TOK_GT_EQ) || (oper == TOK_LT_EQ))
+            code.addOp(OP_NOT, firstOper, firstOper);
+
+        freeReg();
+    }
+}
+
+void Compiler::bitOr()
+{
+    compileDescent(&Compiler::bitXor, TOK_BAR, OP_BIT_OR);
+}
+
+void Compiler::bitXor()
+{
+    compileDescent(&Compiler::bitAnd, TOK_UARROW, OP_BIT_XOR);
+}
+
+void Compiler::bitAnd()
+{
+    compileDescent(&Compiler::shift, TOK_AMP, OP_BIT_AND);
+}
+
+void Compiler::shift()
+{
+    ui8 firstOper = previousReg;
     sum();
+
+    while (consumeToks(TOK_RIGHT_SHIFT, TOK_LEFT_SHIFT))
+    {
+        TokenType oper = previousTok.type;
+        ui8 secondOper = previousReg;
+        sum();
+
+        code.addOp(oper == TOK_RIGHT_SHIFT ? OP_BIT_SHIFT_R
+            : OP_BIT_SHIFT_L, firstOper, firstOper, secondOper);
+        freeReg();
+    }
 }
 
 void Compiler::sum()
@@ -144,7 +330,6 @@ void Compiler::sum()
 
         code.addOp(oper == TOK_PLUS ? OP_ADD : OP_SUB,
             firstOper, firstOper, secondOper);
-
         freeReg();
     }
 }
@@ -152,13 +337,13 @@ void Compiler::sum()
 void Compiler::product()
 {
     ui8 firstOper = previousReg;
-    primary();
+    unary();
 
     while (consumeToks(TOK_STAR, TOK_SLASH, TOK_PERCENT))
     {
         TokenType oper = previousTok.type;
         ui8 secondOper = previousReg;
-        primary();
+        unary();
 
         Opcode op;
         switch (oper)
@@ -169,32 +354,57 @@ void Compiler::product()
             default: UNREACHABLE();
         }
         code.addOp(op, firstOper, firstOper, secondOper);
-        // code.addOp(OP_FREE_R, secondOper);
 
         freeReg();
     }
 }
 
-void Compiler::primary()
+void Compiler::unary()
 {
-    if (consumeTok(TOK_NUM))
+    if (consumeToks(TOK_MINUS, TOK_BANG, TOK_TILDE))
     {
-        Object obj{previousTok.content.i};
-        code.loadRegConst(obj, previousReg);
-        reserveReg();
-    }
+        TokenType oper = previousTok.type;
+        ui8 firstOper = previousReg;
+        exponent();
 
-    else if (consumeTok(TOK_NUM_DEC))
-    {
-        Object obj{previousTok.content.d};
-        code.loadRegConst(obj, previousReg);
-        reserveReg();
+        Opcode op;
+        switch (oper)
+        {
+            case TOK_MINUS: op = OP_NEGATE;     break;
+            case TOK_BANG:  op = OP_NOT;        break;
+            case TOK_TILDE: op = OP_BIT_COMP;   break;
+            default: UNREACHABLE();
+        }
+
+        code.addOp(op, firstOper, firstOper);
+        // We don't free a register since unary
+        // operators don't use any extra registers.
+        // They apply an operator directly onto a
+        // register.
     }
-    
-    else if (consumeTok(TOK_STR_LIT))
+    else
+        exponent();
+}
+
+void Compiler::exponent()
+{
+    compileDescent(&Compiler::primary, TOK_STAR_STAR, OP_POWER);
+}
+
+void Compiler::primary()
+{    
+    if (consumeToks(TOK_NUM, TOK_NUM_DEC, TOK_STR_LIT))
     {
-        HeapObj* ptr = new String(GET_STR(previousTok));
-        Object obj{ptr};
+        Object obj;
+        switch (previousTok.type)
+        {
+            case TOK_NUM:       obj = previousTok.content.i;    break;
+            case TOK_NUM_DEC:   obj = previousTok.content.d;    break;
+            case TOK_STR_LIT:
+                obj = static_cast<HeapObj*>(new String(GET_STR(previousTok)));
+                break;
+            default: UNREACHABLE();
+        }
         code.loadRegConst(obj, previousReg);
         reserveReg();
     }
@@ -211,17 +421,30 @@ void Compiler::primary()
         code.loadReg(previousReg, OP_NULL);
         reserveReg();
     }
+
+    else if (consumeTok(TOK_IDENTIFIER))
+    {
+        ui8* slot = getVarSlot(previousTok);
+        if (slot != nullptr)
+        {
+            code.addOp(OP_GET_VAR, previousReg, *slot);
+            reserveReg();
+        }
+    }
 }
 
 ByteCode& Compiler::compile(const vT& tokens)
 {
     currentTok = tokens[0];
     it = tokens.begin();
+    code.clear();
+
     try
     {
-        while (!checkTok(TOKEN_EOF))
+        while (!checkTok(TOK_EOF))
             declaration();
-        code.addOp(OP_RETURN, static_cast<ui8>(0)); // Temporary register value.
+        if (!tokens.empty())
+            code.addOp(OP_RETURN, static_cast<ui8>(previousReg));
     }
     catch (CompileError& error)
     {
@@ -230,3 +453,5 @@ ByteCode& Compiler::compile(const vT& tokens)
 
     return code;
 }
+
+#endif
