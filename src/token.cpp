ab3434427f1287eae0e6f20a613a416ffcffdd0a
Ali Hamza <ali.mhamza06@gmail.com>
2026-01-21 13:21:00 +0400

    Add proper copy and move constructors/assignment operators to fix data corruption bugs.

--- src/token.cpp
+++ src/token.cpp
@@ -10,3 +10,48 @@ Token::Token(TokenType type, std::string_view text, Value content,
                 ui16 line, ui8 position) :
     text(text), content(content), line(line),
     position(position), type(type) {}
+
+Token::Token(const Token& other) :
+    text(other.text), line(other.line), position(other.position),
+    type(other.type)
+{
+    this->content.i = other.content.i;
+}
+
+Token& Token::operator=(const Token& other)
+{
+    if (this != &other)
+    {
+        this->text = other.text;
+        this->content.i = other.content.i;
+        this->line = other.line;
+        this->position = other.position;
+        this->type = other.type;
+    }
+
+    return *this;
+}
+
+Token::Token(Token&& other) :
+    text(other.text), line(other.line), position(other.position),
+    type(other.type)
+{
+    this->content.i = other.content.i;
+    other.type = TOK_EOF; // Invalidate the other token.
+}
+
+Token& Token::operator=(Token&& other)
+{
+    if (this != &other)
+    {
+        this->text = other.text;
+        this->content.i = other.content.i;
+        this->line = other.line;
+        this->position = other.position;
+        this->type = other.type;
+
+        other.type = TOK_EOF;
+    }
+
+    return *this;
+}
