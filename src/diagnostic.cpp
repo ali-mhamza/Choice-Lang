35f49bb7946619420f6ccec308c31a8893a90234
Ali Hamza <ali.mhamza06@gmail.com>
2026-06-26 04:23:44 +0400

    Handle different function object types for diagnostic display.

--- src/diagnostic.cpp
+++ src/diagnostic.cpp
@@ -608,12 +608,20 @@ std::string DiagnosticEngine::printStackEntry(
     u64 index
 )
 {
-    // The function object is always just before its respective
-    // register window, except script-level code (which has no
-    // function object).
-    const Function* func{
-        (index == 0) ? nullptr : AS_USER_FUNC(frames[index].regStart[-1])
-    };
+    // The function/method/closure object is always just before
+    // its respective register window, except script-level code
+    // (which has no such object).
+    const Function* func{nullptr};
+    if (index != 0)
+    {
+        const Object& obj{frames[index].regStart[-1]};
+        if (IS_METHOD(obj))
+            func = AS_METHOD(obj)->function;
+        else if (IS_CLOSURE(obj))
+            func = AS_CLOSURE(obj)->function;
+        else
+            func = AS_USER_FUNC(obj);
+    }
 
     std::string output{};
     if (index == frames.size() - 1)
